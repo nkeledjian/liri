ab5cb8e6d54d9c13b26596a5ebe2b268f10e0554
nVar <nkeledjian@gmail.com>
2018-11-21 21:47:24 -0800

    Updated key setup

--- liri.js
+++ liri.js
@@ -1,25 +1,19 @@
 // initialize axios
+var keys = require("./keys");
 var axios = require("axios");
+var Spotify = require("node-spotify-api");
+require("dotenv").config();
 
 // variables storing user inputs and utility
 var input1 = process.argv[2];
 var input2 = process.argv[3];
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 // var OMDBkey = new OMDB(keys.OMDB);
 // var bandsKey = new bands(keys.bands);
 
-// var musicInfo = {
-//     "crossDomain": true,
-//     "url": 'https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?market=ES&include_groups=appears_on&limit=2',
-//     "method": 'GET',
-//     "responseType": 'stream',
-//     "headers": {
-//         "Authorization": 'Bearer ' + spotify,
-//     }
-// }
-
+// *** CONCERT THIS COMMAND **
 if (input1 === "concert-this") {
-    axios.get("https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=").then(
+    axios.get("https://rest.bandsintown.com/artists/" + input2 + "/events?app_id="+bandsKey).then(
         function (response) {
             var results = response.data;
             // console.log("----------RESPONSE.DATA----------");
@@ -63,33 +57,20 @@ if (input1 === "concert-this") {
     ); // END Bands in Town API then function
 } // END conditional for concert-this command
 
+
+// *** SPOTIFY THIS SONG COMMAND **
 if (input1 === "spotify-this-song") {
-    axios(musicInfo).then(
-        function (response) {
-            // If the axios was successful...
-            // Then log the body from the site!
-            console.log(response.data);
-        },
-        function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        }
-    ); // END Spotify API then function
+    spotify
+    .search({ type: 'track', query: input2 })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 } // END conditional for spotify-this-song command
 
+// *** MOVIE THIS SONG COMMAND **
 if (input1 === "movie-this") {
     axios.get("http://www.omdbapi.com/?s=" + input2 + "&apikey=" + OMDBkey).then(
         function (response) {
